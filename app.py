5c58959a2283b20ec1df17597e8ca9e2d76e13a9
Koraik <114868665+Koraik@users.noreply.github.com>
2023-05-12 18:48:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """zwróć wiadomomość powitajaca."""
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 if __name__ == '__main__':
